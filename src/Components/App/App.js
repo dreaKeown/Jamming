ccdfe136bc89e56096a33d42611843d35a597be5
dreaKeown <andrea.keown13@gmail.com>
2021-10-07 19:53:15 +0100

    Fourth Commit all components working.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,20 +8,18 @@ import SearchResults from '../SearchResults/SearchResults';
 
 import Playlist from '../Playlist/Playlist';
 
+import Spotify from '../../util/Spotify';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-               searchResults:[{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
-                      {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, 
-                      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+               searchResults:[],
 
                 playlistName: 'My Playlist',
 
-                playlistTracks:[ {name: 'playlistName1', artist: 'artistName1', album: 'album1', id: 4},
-                                 {name: 'playlistName2', artist: 'artistName2', album: 'album2', id: 5},
-                                 {name: 'playlistName3', artist: 'artistName3', album: 'album3', id: 6}]
+                playlistTracks:[]
     }
 
   this.addTrack = this.addTrack.bind(this);
@@ -61,10 +59,18 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(()=> {
+      this.setState ({
+        playlistName: 'New Playlist',
+        playlistTrack: []
+      })
+    })
   }
 
-  search(searchTerm) {
-    console.log(searchTerm);
+  search(term) {
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults})
+    })
   }
 
   render() {
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,38 +3,37 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-    
+  constructor(props) {
+    super(props);
     this.state = {
-        term: ''
+      term: ''
     }
-
     this.search = this.search.bind(this);
-
     this.handleTermChange = this.handleTermChange.bind(this);
+  }
 
-    }
+  search(event) {
+    this.props.onSearch(this.state.term)
+  }
 
-    search() {
-        this.props.onSearch(this.state.term);
-    }
+  handleTermChange(event) {
+    this.setState({term: event.target.value})
+  }
 
-    handleTermChange(event) {
-        this.setState({term: event.target.value});
-    }
-    render() {
-        return (
-            <div className="SearchBar">
-                <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist"  />
-                <button className="SearchButton">SEARCH</button>
-            </div>  
-
-            )
-        }
-
-    }
-
-
+  render() {
+    return (
+      <div className="SearchBar">
+        <input 
+          placeholder="Enter A Song, Album, or Artist" 
+          onChange={this.handleTermChange}
+        />
+        <button 
+          className="SearchButton"
+          onClick={this.search}
+        >SEARCH</button>
+      </div>
+    )
+  }
+}
 
 export default SearchBar;
