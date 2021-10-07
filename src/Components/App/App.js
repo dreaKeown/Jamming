714abde0167b0c2d18ae4d91de768f90d1f3d92a
dreaKeown <andrea.keown13@gmail.com>
2021-10-07 13:09:33 +0100

    Third Commit search bar complete

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -27,6 +27,12 @@ class App extends React.Component {
   this.addTrack = this.addTrack.bind(this);
 
   this.removeTrack = this.removeTrack.bind(this);
+
+  this.updatePlaylistName = this.updatePlaylistName.bind(this);
+
+  this.savePlaylist = this.savePlaylist.bind(this);
+
+  this.search = this.search.bind(this);
   
   }
 
@@ -49,6 +55,18 @@ class App extends React.Component {
 
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name});
+  }
+
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  search(searchTerm) {
+    console.log(searchTerm);
+  }
+
   render() {
     return (
       <div>
@@ -56,7 +74,7 @@ class App extends React.Component {
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
 
           <div className="App">
-            <SearchBar />
+            <SearchBar onSearch = {this.search} />
 
             <div className="App-playlist">
               <SearchResults searchResults={this.state.searchResults} 
@@ -65,7 +83,11 @@ class App extends React.Component {
 
                         playlistTracks={this.state.playlistTracks}
                         
-                        onRemove={this.removeTrack} />
+                        onRemove={this.removeTrack} 
+                        
+                        onNameChange = {this.updatePlaylistName}
+                        
+                        onSave = {this.savePlaylist}/>
             
             </div>
 
