95d467848614cac4676114c553ec55f140ac13a0
dreaKeown <andrea.keown13@gmail.com>
2021-10-08 07:45:18 +0100

    Fifth Commit completed

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,41 +1,32 @@
-import React from "react";
-
+import React from 'react';
+import TrackList from '../TrackList/TrackList.js';
 import './Playlist.css';
-
-import TrackList from "../TrackList/TrackList"; 
 
 class Playlist extends React.Component {
     constructor(props) {
         super(props);
 
-    this.handleNameChange=this.handleNameChange.bind(this);
-
+        this.handleNameChange = this.handleNameChange.bind(this);
     }
 
     handleNameChange(event) {
         this.props.onNameChange(event.target.value);
-
     }
-    
-    
+
     render() {
         return (
             <div className="Playlist">
-
-                <input defaultValue={'New Playlist'}
-                        
-                       onChange={this.handleNameChange} />
-
-                    <TrackList tracks={this.props.playlistTracks} 
-                    
-                        onRemove = {this.props.onRemove}
-                        
-                        isRemoval = {true} />
-
+                <input value={this.props.playlistName} onChange={this.handleNameChange} />
+                {<TrackList tracks={this.props.playlistTracks}
+                    onRemove={this.props.onRemove}
+                    isRemoval={true} />}
                 <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
             </div>
-        )
+        );
     }
-}
+};
 
 export default Playlist;
+
+
+
